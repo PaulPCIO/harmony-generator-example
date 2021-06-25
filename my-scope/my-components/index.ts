326cba62556c209a7a986b5347c1b825dfd24898
Paul van Dyk <paul@vandyk.fr>
2021-06-25 14:14:09 +0200

    commit example

---
+++ my-scope/my-components/index.ts
@@ -0,0 +1,5 @@
+import { MyComponentsAspect } from './my-components.aspect';
+
+export type { MyComponentsMain } from './my-components.main.runtime';
+export default MyComponentsAspect;
+export { MyComponentsAspect };
---
+++ my-scope/my-components/my-components.aspect.ts
@@ -0,0 +1,6 @@
+import { Aspect } from '@teambit/harmony';
+
+export const MyComponentsAspect = Aspect.create({
+  id: 'my-scope/my-components',
+});
+  
